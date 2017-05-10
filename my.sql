25c422023be1a234f7f9ebef5f6c72491acb58a3
hrtzmx <917377862@qq.com>
2017-05-10 20:49:24 +0800

    add jquery file

---
+++ my.sql
@@ -0,0 +1,14 @@
+SET NAMES UTF8;
+DROP DATABASE IF EXISTS taobao;
+CREATAE DATABASE taobao CHARSET=UTF8;
+USE taobao;
+CREATE TABLE product(
+pid INT PRIMARY KEY AUTO_INCREMENT,
+pconent VARCHAR(32)
+);
+INSERT INTO product VALUES
+(NULL,'dhddjdjjj'),
+$_REQUEST['ID'] OR DIE("ID REQUIRED")
+$conn=mysqli_connect('127.0.0.1','root','','taobao',3306);
+$sql="set names utf8";
+mysqli_query($conn,$sql)5
